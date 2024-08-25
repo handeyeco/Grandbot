740049bead9fc29ddff6615f138182dfb6313101
handeyeco <matthew.c.tx@gmail.com>
2024-08-25 08:26:25 -0500

    change buttons stuff to bools

--- src/Button.cpp
+++ src/Button.cpp
@@ -11,19 +11,19 @@ void Button::read() {
   bool read = digitalRead(pin);
 
   history = history << 1;
-  history |= (read == 0);
+  history |= (read == false);
 
-  pressed = 0;
-  released = 0;
+  pressed = false;
+  released = false;
 
   // handle press
   if ((history & BUTTON_MASK) == 0b00000111) {
-      pressed = 1;
+      pressed = true;
       history = 0b11111111;
   }
   // handle release
   else if ((history & BUTTON_MASK) == 0b11000000) {
-    released = ignoreRelease ? 0 : 1;
+    released = ignoreRelease ? false : true;
     ignoreRelease = false;
     history = 0b00000000;
   }
